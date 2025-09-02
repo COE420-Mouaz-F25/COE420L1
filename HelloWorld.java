91dc27967275484493feb71234b80d08896634f6
Ibrahim-cmp <b00096538@aus.edu>
2025-09-02 20:56:21 +0400

    Feature B added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        featureB();
+    }
+
+    public static void featureB()
+    {
+        System.out.println("This is feature B.");
     }
 }
